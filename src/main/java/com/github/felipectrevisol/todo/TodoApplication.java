f4e1d9b2195ef4e174a22e85ef7e98ae543d53ad
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2025-08-27 21:26:50 -0300

    feat: Adicionado a anotação EnableAsync.

--- src/main/java/com/github/felipectrevisol/todo/TodoApplication.java
+++ src/main/java/com/github/felipectrevisol/todo/TodoApplication.java
@@ -1,8 +1,10 @@
 package com.github.felipectrevisol.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAsync
 @SpringBootApplication 
 public class TodoApplication {
 
