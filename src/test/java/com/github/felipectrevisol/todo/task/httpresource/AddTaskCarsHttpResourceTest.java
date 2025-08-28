371c7caaf20b416a785bca8f7495c986cce11afc
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2025-08-27 21:55:25 -0300

    fix: Adicionada a injeção de dependência para TaskCardAdder no Teste Unitário.

--- src/test/java/com/github/felipectrevisol/todo/task/httpresource/AddTaskCarsHttpResourceTest.java
+++ src/test/java/com/github/felipectrevisol/todo/task/httpresource/AddTaskCarsHttpResourceTest.java
@@ -2,6 +2,8 @@ package com.github.felipectrevisol.todo.task.httpresource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import com.github.felipectrevisol.todo.task.TaskCardAdder;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -11,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AddTaskCarsHttpResourceTest {
 
 	@Autowired MockMvc mockMvc;
+	@MockBean TaskCardAdder adder;
 
 	@Test void postRequest() throws Exception {
 		mockMvc.perform(post("/task")
