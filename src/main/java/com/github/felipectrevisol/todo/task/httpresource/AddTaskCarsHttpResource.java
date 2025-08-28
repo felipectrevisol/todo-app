f287c7efa39fe663e231a81ffc566d11fcd8db82
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2025-08-27 21:53:38 -0300

    feat: Integrada a classe AddTaskCarsHttpResource com a classe TaskCardAdder.

--- src/main/java/com/github/felipectrevisol/todo/task/httpresource/AddTaskCarsHttpResource.java
+++ src/main/java/com/github/felipectrevisol/todo/task/httpresource/AddTaskCarsHttpResource.java
@@ -1,6 +1,7 @@
 package com.github.felipectrevisol.todo.task.httpresource;
 
 import org.springframework.http.HttpStatus;
+import com.github.felipectrevisol.todo.task.TaskCardAdder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/task")
 public class AddTaskCarsHttpResource {
+
+    private TaskCardAdder adder;
+
+    public AddTaskCarsHttpResource(final TaskCardAdder adder) {
+        this.adder = adder;
+    }
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void post(@RequestBody HttpBody httpBody) {
-        System.out.println(httpBody.title());
-        System.out.println(httpBody.priority());
+        this.adder.add(new com.github.felipectrevisol.todo.task.TaskCard(httpBody.title(), httpBody.priority()));
     }
 }
